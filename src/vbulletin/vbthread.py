4fc9678a05705e1b059933d1ff31793d4af75b97
driscoll <driscollkevin@gmail.com>
2010-06-25 16:06:57 -0700

    Major cleanup and refactoring of Thread class

--- src/vbulletin/vbthread.py
+++ src/vbulletin/vbthread.py
@@ -11,93 +11,78 @@
 import re
 import sys
 import urllib2
+import vbutils
 
-class Devnull:
-    """Destination for msgs in non-verbose
-    """
-    def write(self, msg):
-        pass
+def getFirstPage(url=''):
+    """Download html from first page of a thread"""
+    if url:
+        # TODO catch errors
+        response = urllib2.urlopen(url)
+        return response.read()
+    return None
+
+def scrapeForumName(html):
+    """Scrape forum name out of HTML"""
+    # TODO Relying on vB convention, need to template
+    if html:
+        titletag = html[(html.find('<title>')+7):html.find('</title>')].strip()
+        return titletag.split('-').pop().strip()
+    else:
+        return ''
+
+def scrapeTitle(html):
+    """Scrape thread title out of HTML"""
+    # TODO Relying on vB convention, need to template
+    title = ''
+    if html:
+        titletag = html[(html.find('<title>')+7):html.find('</title>')].strip()
+        for t in titletag.split('-')[0:-1]:
+            title += t.strip() + '_'
+    if title:
+        return title[:-1]
+    return ''
+
+def scrapeNumPages(html):
+    """Scrape number of pages from thread"""
+    # TODO based on vB convention, need template
+    if html:
+        m = re.search(r'Page [0-9]* of ([0-9]*)', html)
+        if m:
+            return int(m.group(1).strip())
+    return 1
 
 class Thread:
     """A thread is a collection of posts
+        Thread objects can be built with either
+            * unique thread ID # or
+            * url to some page in the thread
     """
+
+    def update(self, url = ''):
+        """Retrieve HTML from first page and scrape basic info 
+        """
+   
+        if not url:
+            url = self.url 
  
-    def __init__(self, url):
-        
-        # TODO this is janky debug code for handling output.
-        # Probably need some kind of output handler object?
-        # VERBOSE = Devnull()
-        VERBOSE = sys.stdout
+        self.url = vbutils.cleanURL(url)
+        self.id = vbutils.findThreadID(self.url)
+        html = getFirstPage(self.url)
+        self.forum = vbutils.makeSlug(scrapeForumName(html))
+        self.title = vbutils.makeSlug(scrapeTitle(html))
+        self.numpages = scrapeNumPages(html)
 
-        self.url = url
+    def __init__(self, url=''):
 
-        # TODO Not sure how this will be used yet
-        # But for now, it's a list of datetimes corresponding
-        # to subdirs for each time that the thread is downloaded 
         self.archive = {} 
-
-        # Extract unique thread ID from the URL
-        print >>VERBOSE, "Seeking thread ID ..."
-        m = re.search(r't=([0-9]*)', self.url)
-        if m == None:
-            print 'Error: Could not locate thread ID in URL.'
-            print
-            sys.exit(2)
-        else:
-            self.id = m.group(1).strip()
-        print >>VERBOSE, "Thread ID: %s" % self.id
-
-        # Get the HTML of the first page in the thread
-        # Generate urllib2 request object from the URL
-        req = urllib2.Request(url)
-        # Get the HTML of the first page in the thread
-        # TODO catch errors
-        response = urllib2.urlopen(url)
         
-        self.html = response.read()
-
-        response.close()
-
-        # Locate contents of title
-        # Split the resulting string by -
-        #   TODO this should be templated 
-        title = re.sub(r'[^a-zA-Z0-9-]',r'_',
-            self.html[(self.html.find('<title>')+7):
-            self.html.find('</title>')]).lower().split('-')
+        self.forum = '' 
+        self.id = ''
+        self.numpages = 1 
+        self.title = ''
+        self.url = url
         
-        # Pop forum name and create slug to use in filenames
-        # TODO Relying on vB convention, need to template
-        # TODO can people edit thread titles? if so, better to use ID# than slug?
-        print >>VERBOSE, "Discovering forum name ..."
-        self.forum = title.pop().strip('_')
-        print >>VERBOSE, "Forum name: %s" % self.forum
-                 
-        # Locate thread title and generate slug to use in filenames
-        # * Concat 32 chars
-        # * Trim trailing spaces
-        # * Convert non-alphanumeric to underscores
-        # * Convert all alpha to lowercase
-        # e.g. 
-        #   <title> Israel raids ships carrying aid to Gaza, killings civilians - Page 25 - EXTREMESKINS.com</title>
-        # becomes
-        #   israel_raids_ships_carrying_aid
-        print >>VERBOSE, "Generating nickname for this thread from title..."
-        self.slug = ''
-        for t in title:
-            self.slug += t.strip('_') + '_'
-        # Trim the slug down to 32 chars
-        self.slug = self.slug[0:32].strip('_')    
-        print >>VERBOSE, "Thread nickname: %s" % self.slug
-
-        # Discover if this thread is multi-page
-        print >>VERBOSE, "Checking length of %s ..." % self.slug
-        # re.search() returns None if there are no matches
-        # TODO template the Page numbers interface
-        m = re.search(r'Page [0-9]* of ([0-9]*)', self.html)
-        if (m == None):
-            self.numpages = 1
-        else:
-            self.numpages = int(m.group(1).strip())
-        print >>VERBOSE, "Found %s page(s) of posts." % self.numpages
+        if url:
+            print "ok"
+            self.update(url)
  
-
