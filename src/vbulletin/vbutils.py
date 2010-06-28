9de8c6d044eebfebed903092db6ea5890d5e4b3f
driscoll <driscollkevin@gmail.com>
2010-06-28 11:14:12 -0700

    Created utility funcs to write JSON files, work w dates

--- src/vbulletin/vbutils.py
+++ src/vbulletin/vbutils.py
@@ -1,7 +1,37 @@
 #!/bin/python
 
+from datetime import datetime
+from dateutil.parser import *
+
 import re
 
+def isDate(s):
+    """Check if a string is a formatted datetime
+    """
+    if (type(s) == str):
+        if (len(s) == 15):
+            return (s[8] == 'T')
+    return False
+
+def getDateTime(utc = 0):
+    """Return machine- and human-readable date-time as string, e.g. 20070304T203217
+    """
+    if utc:
+        return datetime.utcnow().strftime('%Y%m%dT%H%M%S')
+    else:
+        return datetime.now().strftime('%Y%m%dT%H%M%S')
+
+def convertDateTime(messydate):
+    """Return arbitrary str to formatted datetime, '%Y%m%dT%H%M%S'
+   
+    e.g. 
+    messydate = 'June-4th-2010, 10:48 AM'
+    returns '20100604T104800' 
+
+    TODO This will be an on-going problem because vB 
+    can displays dates in a variety of ways"""
+    return parse(messydate, fuzzy = True).strftime('%Y%m%dT%H%M%S')
+    
 def findThreadID(s):
     """Find thread ID in a URL or filename"""
     m = re.search(r't=([0-9]*)', s)
@@ -9,6 +39,23 @@ def findThreadID(s):
         return None 
     else:   
         return m.group(1).strip()
+
+def findThreadURL(s, id):
+    """Find thread URL for given ID in a string"""
+    # This pattern should match the first page in the thread
+    # TODO this pattern isn't working
+    pattern = r'http://[^\'"]*showthread[^\'"]*t=%s[^\'"]*' % id
+    m = re.search(pattern, s)
+    if m == None:
+        return None 
+    else:
+        return m.group(0).strip()
+
+def findThreadURLs(s):
+    """Return all valid thread URLs in a string"""
+    # This pattern should match the first page in threads
+    pattern = r'http://[^\'"]*showthread[^\'"]*t=[0-9]*[^\'"]*'
+    return re.findall(pattern, s)
 
 def isValidURL(url):
     # TODO This is janky verification but it's a start
@@ -23,7 +70,7 @@ def cleanURL(raw_URL):
     # Or... drop all parameters except for &t=
     # TODO are there parameters worth keeping?
     url = ''
-    if isValidURL(url):
+    if isValidURL(raw_URL):
         url = re.sub(r'[&@?][^t][a-zA-Z]*=[a-zA-Z0-9]*','',raw_URL)
         if not (url.startswith('http://')):
             url = 'http://' + url
@@ -43,18 +90,23 @@ def makeSlug(s, length = 32):
     slug = re.sub(r'[^a-zA-Z0-9-]', r'_', s).lower().strip('_')
     return slug[0:(length - 1)].strip('_')
 
-
-    for t in title:
-        slug += t.strip('_') + '_'
-        # Trim the slug down to 32 chars
-        slug = slug[0:32].strip('_')    
-    return slug
-
 class Devnull:
     """Destination for msgs in non-verbose
     """
     def write(self, msg):
         pass
+
+def dumpf(obj, filename = ''):
+    """Dump a Post or Thread object to a JSON file""" 
+    # TODO Should check if obj is Post or Thread
+    if not filename:
+        filename = obj.title + '.json'
+    print "Printing JSON data to %s ..." % filename
+    # TODO catch errors
+    j = obj.exportJSON(4)
+    print j
+    with open(filename, 'w') as f:
+        f.write(j)
 
 
 
